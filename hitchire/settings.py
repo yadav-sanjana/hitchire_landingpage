19bd4c63da512bf193a12cbc79fb40b263538a4e
yadav-sanjana <sanjanay751@gmail.com>
2023-08-20 19:31:24 +0530

    a rough landing page visual

--- hitchire/settings.py
+++ hitchire/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hitchire_app'
+    'hitchire_app',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS':  [os.path.join(BASE_DIR, 'templates')],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- hitchire_app/static/js/script.js
+++ hitchire_app/static/js/script.js
@@ -1,4 +1,44 @@
-// landingpage_app/static/js/scripts.js
-document.getElementById('click-me').addEventListener('click', () => {
-    document.getElementById('message').textContent = 'Button clicked!';
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.querySelector('.menu-button');
+    const signinButton = document.querySelector('.signin');
+    const signupButton = document.querySelector('.signup');
+
+    // Menu button click event
+    menuButton.addEventListener('click', function () {
+        // Add your menu toggle logic here
+        alert('Menu button clicked!');
+    });
+
+    // Sign In button click event
+    signinButton.addEventListener('click', function () {
+        // Add your sign-in logic here
+        alert('Sign In clicked!');
+    });
+
+    // Sign Up button click event
+    signupButton.addEventListener('click', function () {
+        // Add your sign-up logic here
+        alert('Sign Up clicked!');
+    });
 });
+
+// Get all banner images
+const bannerImages = document.querySelectorAll('.banner-image');
+const bannerStepper = document.querySelector('.banner-stepper');
+
+let currentImageIndex = 0;
+
+// Function to toggle active class on images
+function toggleImages() {
+    bannerImages[currentImageIndex].classList.remove('active');
+    currentImageIndex = (currentImageIndex + 1) % bannerImages.length;
+    bannerImages[currentImageIndex].classList.add('active');
+}
+
+// Set interval for image change
+setInterval(() => {
+    toggleImages();
+}, 1000);
+
+// Add animation class to the banner stepper
+bannerStepper.classList.add('animate');
